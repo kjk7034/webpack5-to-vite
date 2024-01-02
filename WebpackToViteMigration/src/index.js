687622f25c2e5ca7cbee71f037cc99109b3c27ff
wise7034 <wise7034@gmail.com>
2024-01-02 11:39:29 +0900

    chore: move webpack project

---
+++ WebpackToViteMigration/src/index.js
@@ -0,0 +1,13 @@
+/* eslint-env browser */
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
